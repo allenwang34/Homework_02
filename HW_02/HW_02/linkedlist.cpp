b1c5bffe54f69f2ad82c1c146451efe08d8486e0
Yiyang Wang Mac <allenwang950321@gmail.com>
2017-07-16 16:08:48 -0700

    a good start of linkedList.

--- HW_02/HW_02/linkedlist.cpp
+++ HW_02/HW_02/linkedlist.cpp
@@ -16,33 +16,56 @@ typedef string ItemType;
 struct Node {
     ItemType value;
     Node *next;
+    
+    Node (ItemType d) {
+        next = nullptr;
+        value = d;
+    }
 };
 
 class LinkedList {
     
 private:
     Node *head;
+    int linkedListSize;
     
 public:
     
     // default constructor
-    LinkedList() : head(nullptr) { }
+    LinkedList() : head(nullptr) {
+        linkedListSize = 0;
+    }
     
     // copy constructor
     LinkedList(const LinkedList& rhs);
     
     // Destroys all the dynamically allocated memory
     // in the list.
-    ~LinkedList();
+    ~LinkedList() {
+        Node *next = head;
+        Node *prev = nullptr;
+        for ( ; next!=nullptr; next = next->next)
+            prev = next;
+            delete prev;
+    }
     
     // assignment operator
     const LinkedList& operator=(const LinkedList& rhs);
     
     // Inserts val at the front of the list
-    void insertToFront(const ItemType &val);
+    void insertToFront(const ItemType &val) {
+        Node *newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+        linkedListSize++;
+    }
     
     // Prints the LinkedList
-    void printList() const;
+    void printList() const {
+        for (Node *p = head; p!= nullptr; p = p->next) {
+            cout << p->value << endl;
+        }
+    }
     
     // Sets item to the value at position i in this
     // LinkedList and return true, returns false if
@@ -63,7 +86,24 @@ public:
     void swap(LinkedList &other);
     
     // Returns the number of items in the Linked List.
-    int size() const;
+    int size() const {
+        return linkedListSize;
+    }
 };
+
+
+
+
+
+
+int main() {
+    LinkedList samplelist;
+    for (int i = 0; i < 3; i++) {
+        string a = "hello";
+        samplelist.insertToFront(a);
+    }
+    cout << samplelist.size() << endl;
+    samplelist.printList();
+}
 
 
